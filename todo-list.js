bf4741ba9384a807a4c62aec72a6b64b075e5d91
whosinghajay <140946442+whosinghajay@users.noreply.github.com>
2023-08-22 23:21:38 +0530

    Add files via upload

---
+++ todo-list.js
@@ -0,0 +1,39 @@
+const todoList=[{name:'playGame', dueDate:'2022-12-22'},{name:'playHockey', dueDate:'2022-12-22'}];
+
+renderTodoList();
+function renderTodoList(){
+    let todoListHTML='';
+
+    for(let i=0;i<todoList.length;i++){
+        const todoObject=todoList[i];
+        // const name=todoObject.name;
+        // const dueDate=todoObject.dueDate;
+        const { name, dueDate}=todoObject;
+        const html=`
+        <div>${name}</div> <div>${dueDate}</div>
+        <button onclick="
+        todoList.splice(${i}, 1);
+        renderTodoList();
+        " class="delete-todo-button">Delete</button>`;
+        todoListHTML += html;
+    }
+    //console.log(todoListHTML);
+
+    document.querySelector('.js-todo-list').innerHTML=todoListHTML;
+    }
+
+function addTodo() {
+    const inputElement = document.querySelector('.js-name-input');
+    const name = inputElement.value;
+    const dateInputElement=document.querySelector('.js-due-date-input');
+    const dueDate = dateInputElement.value;
+    todoList.push(
+        {
+        //  name:name,
+        //  dueDate:dueDate
+         name,dueDate
+        }
+        );
+    inputElement.value = ''; 
+    renderTodoList();
+}
